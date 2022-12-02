c9b3fb0e48307b57c1c1e0670cb41a495d426d6c
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-12-02 18:40:40 +0100

    Back button in View

--- src/routes/View.svelte
+++ src/routes/View.svelte
@@ -1,9 +1,12 @@
 <script>
 	import downloadSvgIcon from '../assets/icons/download.svg'
+	import backSvgIcon from '../assets/icons/back.svg'
+	import deleteSvgIcon from '../assets/icons/delete.svg'
 	import ErrorMessage from '../components/ErrorMessage.svelte'
 	import * as htmlToImage from 'html-to-image'
 	import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image'
 	import download from 'downloadjs'
+	import { navigate } from 'svelte-routing'
 
 	export let id
 	export let loggedIn
@@ -33,6 +36,10 @@
 		} else {
 			throw new Error('Impossibile fare richiesta al server')
 		}
+	}
+
+	async function navigateBack() {
+		navigate('/')
 	}
 
 	async function downloadInvoice() {
@@ -123,8 +130,15 @@
 			</div>
 		</div>
 
+		<!-- Back button -->
+		<div class="fixed z-10 top-18 left-4">
+			<button class="btn rounded-full w-12 px-0" on:click={navigateBack}>
+				<img src={backSvgIcon} class="h-5 w-5" alt="Back button icon" />
+			</button>
+		</div>
+
 		<!-- Create invoice button -->
-		<div class="fixed z-10 bottom-4 right-4">
+		<div class="fixed z-10 bottom-20 right-4">
 			<button
 				class="btn rounded-full w-12 px-0 bg-emerald-700 hover:bg-emerald-800 focus:bg-emerald-800 border-transparent hover:border-transparent focus:border-transparent"
 				on:click={downloadInvoice}
@@ -134,6 +148,15 @@
 					class="h-5 w-5"
 					alt="Download invoice icon"
 				/>
+			</button>
+		</div>
+
+		<!-- Delete invoice button -->
+		<div class="fixed z-10 bottom-4 right-4">
+			<button
+				class="btn rounded-full w-12 px-0 bg-red-700 hover:bg-red-800 focus:bg-red-800 border-transparent hover:border-transparent focus:border-transparent"
+			>
+				<img src={deleteSvgIcon} class="h-5 w-5" alt="Delete invoice icon" />
 			</button>
 		</div>
 	</div>
