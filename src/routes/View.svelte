5d3ced26df1a7ccc55c0daef4e76216cf5385848
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-12-07 01:11:47 +0100

    Implemented view feature Amazon S3

--- src/routes/View.svelte
+++ src/routes/View.svelte
@@ -1,4 +1,6 @@
 <script>
+	import config from '../../config.json'
+
 	import downloadSvgIcon from '@assets/icons/download.svg'
 	import backSvgIcon from '@assets/icons/back.svg'
 	import deleteSvgIcon from '@assets/icons/delete.svg'
@@ -7,9 +9,8 @@
 	import download from 'downloadjs'
 	import { navigate } from 'svelte-routing'
 
-	import { getUser, getDocument } from '@src/api'
 	import { loggedIn } from '@src/stores';
-
+	
 	export let id
 	export let sitename
 
@@ -24,6 +25,17 @@
 			download(dataUrl, 'fattura.png')
 		})
 	}
+
+	async function viewInvoice() {
+		const req = await fetch(`${config.API_BASE_URL}/invoices/view/${id}`, {
+			credentials: 'include',
+		})
+
+		if (req.ok) {
+			const url = await req.json()
+			return url
+		}
+	}
 </script>
 
 <svelte:head>
@@ -33,76 +45,14 @@
 {#if $loggedIn}
 	<div class="flex justify-center">
 		<div
-			class="w-screen max-w-4xl bg-white shadow-md rounded-md p-8 aspect-[210/297]"
+			class="w-screen max-w-4xl bg-white shadow-md rounded-md aspect-[210/297]"
 			id="invoice"
 		>
-			<div class="text-gray-600">
-				{#await getUser() then azienda}
-					<i class="font-serif"
-						><h1 class="text-xl font-bold uppercase">{azienda.nomeAzienda}</h1>
-						<p>{azienda.indirizzoResidenzaFiscale}</p>
-						<p>{azienda.indirizzoResidenzaCitta}</p>
-						<p class="mb-12">P.I. {azienda.pIva}</p></i
-					>
-				{:catch err}
-					{err}
-				{/await}
-
-				{#await getDocument(id) then fattura}
-					<div
-						class="bg-cyan-600 flex flex-row text-white py-2 px-4 mb-4 font-medium"
-					>
-						<p class="flex-1">Fattura N° {fattura.numeroFattura}</p>
-						<p class="flex-1 text-right">30/10/22</p>
-					</div>
-
-					<p class="border-b-2 border-cyan-600 pb-1 mb-4">Fatturare a</p>
-					<p class="font-serif uppercase mb-2">{fattura.nominativo}</p>
-					<p class="font-serif uppercase"><b>Via</b></p>
-					<p class="font-serif uppercase">
-						{fattura.indirizzoResidenza}
-						{fattura.capResidenza}
-					</p>
-					<p class="font-serif uppercase mb-2">{fattura.cittaResidenza}</p>
-					{#if fattura.tipo === 'azienda'}
-						<p class="font-serif uppercase"><b>P.I.</b></p>
-						<p class="font-serif uppercase mb-12">{fattura.pIva}</p>
-					{/if}
-					{#if fattura.tipo === 'privato'}
-						<p class="font-serif uppercase"><b>Cod. Fiscale</b></p>
-						<p class="font-serif uppercase mb-12">{fattura.codiceFiscale}</p>
-					{/if}
-
-					<div class="bg-cyan-600 flex flex-row text-white py-2 px-4 mb-4">
-						<p class="flex-1 font-medium">COMPENSO PROFESSIONALE PER:</p>
-					</div>
-
-					<p class="border-b-2 font-serif border-cyan-600 pb-1 mb-6">
-						{fattura.lavoroSvolto}
-					</p>
-
-					<div class="flex flex-row text-gray-800 mb-40">
-						<div class="flex-[4_4_0%]">
-							<p class="font-bold text-right">TOTALE</p>
-						</div>
-						<div class="flex-[3_3_0%]">
-							<p class="font-bold text-right">{fattura.totale}€</p>
-						</div>
-						<div class="flex-1"><p>&nbsp;</p></div>
-					</div>
-				{:catch err}
-					{err}
-				{/await}
-
-				<p class="font-serif">
-					Operazione effettuata ex art. 1 c. 54-89 L. 190/2014 così come
-					modificato dalla Legge n. 208/2015. Regime forfettario in franchigia
-					di iva e non soggetta a ritenuta d'acconto - Imposta di bollo da 2
-					euro assolta sull'originale per importi maggiori di 77,47 euro.
-					<br /><br />
-					Marca da bollo id:
-				</p>
-			</div>
+			{#await viewInvoice()}
+				Loading invoice...
+			{:then url} 
+				<img src={url} alt="Visualizza fattura">
+			{/await}
 		</div>
 
 		<!-- Back button -->
