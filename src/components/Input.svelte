fb846480e0202b0b45afcec58dd822a28febaa08
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-12-14 22:24:50 +0100

    Added input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,21 @@
+<script>
+    let input
+    export let type
+    export let placeholder
+    export let value = ''
+    export let name
+    export let required = false
+
+    const handleInput = () => { value = input.value }
+</script>
+
+<input
+    bind:this={input}
+    type={type}
+    name={name}
+    placeholder={placeholder}
+    id={name}
+    on:input={handleInput}
+    class="input w-full input-bordered max-w-sm"
+    required={required}
+/>
